6fd1ed0300627e53fde9bc6e9a5f4b329a78f21f
Gerald Nqobile Ndlovu <157401184+GeraldNdlovu@users.noreply.github.com>
2024-06-10 22:22:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+from flask import Flask, render_template
+import pandas_datareader.data as web
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    start = datetime(2023, 9, 23)
+    end = datetime(2023, 10, 23)
+    df = web.DataReader('AAPL', 'yahoo', start, end)
+    return render_template('index.html', data=df.to_html())
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
